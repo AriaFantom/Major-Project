4dfbb15eacfb2f8681045ac4d11c4af2ddc41259
AriaFantom <priyanshudebnath348@gmail.com>
2025-04-26 00:43:04 +0530

    updated some files

---
+++ src/main/java/com/project/bizconnect/entity/Role.java
@@ -0,0 +1,9 @@
+package com.project.bizconnect.entity;
+
+public enum Role {
+
+    CUSTOMER,
+    SELLER,
+    ADMIN,
+
+}
---
+++ src/main/java/com/project/bizconnect/entity/User.java
@@ -0,0 +1,26 @@
+package com.project.bizconnect.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+}
